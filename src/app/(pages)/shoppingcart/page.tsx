86f1d8709711d2ccb88833e4126e9a96da4cca38
Joseph Leung <leung.joseph@outlook.com>
2024-10-05 14:18:49 -0400

    Dynamic items page finished

--- src/app/(pages)/shoppingcart/page.tsx
+++ src/app/(pages)/shoppingcart/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Shoppingcart() {
-  return <div>Shoppingcart</div>;
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <div className="text-xl">COMING SOON...</div>
+    </div>
+  );
 }
