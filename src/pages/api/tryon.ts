3a123440a3eafad3635e7203ed29681d4135b1b4
danielp1218 <danielpu2007@gmail.com>
2024-10-06 09:51:07 -0400

    oopsx2

--- src/pages/api/tryon.ts
+++ src/pages/api/tryon.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     // Prepare payload for the Stable Diffusion API
     const payload = {
-      key: "Ge9g93nJzq8Z8wr8nv4yXMMBkvYy467Pd90e9a0XXR6uvlxBI1eP5UXxdVYG",
+      key: "SJeyQrKd0m1Xt63jvKlS3zDKWjNWcmoVuQuRtP1P2wLLpdSJXOVc7HggFVtg",
       prompt: "A realistic photo",
       negative_prompt: "Low quality, unrealistic, bad cloth, warped cloth",
       init_image: personImageUrl,
