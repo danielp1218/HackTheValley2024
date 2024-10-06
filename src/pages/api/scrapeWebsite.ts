43468a7376575ede9bcbb207382ed176ce0e0fb1
Joseph Leung <leung.joseph@outlook.com>
2024-10-06 06:59:19 -0400

    fixed search bar

--- src/pages/api/scrapeWebsite.ts
+++ src/pages/api/scrapeWebsite.ts
@@ -1,7 +1,7 @@
 import {scrape} from "@/utils/scrape";
 
 export default async function handler(req, res) {
-    const url = req.query.url;
+    const { url } = req.body;
     const result = await scrape(url);
     res.status(200).json(result);
 }
